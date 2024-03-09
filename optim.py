5871c324371dbf5d3b46b8a5fe5840d5cec306c8
SedGram <shauryajnvkkr@gmail.com>
2024-03-09 18:16:52 +0530

    Currently Supports CAME , Lion , SM - II

---
+++ optim.py
@@ -0,0 +1,465 @@
+import math
+import torch
+from torch.optim import Optimizer
+
+class SM3(Optimizer):
+    """Implements SM3 algorithm.
+    Adapted from https://github.com/Enealor/PyTorch-SM3/
+    It has been proposed in `Memory-Efficient Adaptive Optimization`_.
+    Adam and Adagrad use O(nm) memory for history tensors, while SM3 uses O(n+m)
+
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): coefficient that scale delta before it is applied
+            to the parameters (default: 0.1)
+        momentum (float, optional): coefficient used to scale prior updates
+            before adding. This drastically increases memory usage if
+            `momentum > 0.0`. This is ignored if the parameter's gradient
+            is sparse. (default: 0.0)
+        beta (float, optional): coefficient used for exponential moving
+            averages (default: 0.0)
+        eps (float, optional): Term added to square-root in denominator to
+            improve numerical stability (default: 1e-30)
+
+    .. _Memory-Efficient Adaptive Optimization:
+        https://arxiv.org/abs/1901.11150
+    """
+    def __init__(self, params, lr=0.1, momentum=0.0, beta=0.0, eps=1e-30):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {0}".format(lr))
+        if not 0.0 <= momentum < 1.0:
+            raise ValueError("Invalid momentum: {0}".format(momentum))
+        if not 0.0 <= beta < 1.0:
+            raise ValueError("Invalid beta: {0}".format(beta))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid eps: {0}".format(eps))
+
+        defaults = {'lr': lr, 'momentum': momentum, 'beta': beta, 'eps': eps}
+        super(SM3, self).__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            momentum = group['momentum']
+            beta = group['beta']
+            eps = group['eps']
+            for p in group['params']:
+                grad = p.grad
+                if p is None or grad is None:
+                    continue
+
+                state = self.state[p]
+                shape = grad.shape
+                rank = len(shape)
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['momentum_buffer'] = 0.
+                    _add_initial_accumulators(state, grad)
+
+                if grad.is_sparse:
+                    # the update is non-linear so indices must be unique
+                    grad.coalesce()
+                    grad_indices = grad._indices()
+                    grad_values = grad._values()
+
+                    # Transform update_values into sparse tensor
+                    def make_sparse(values):
+                        constructor = grad.new
+                        if grad_indices.dim() == 0 or values.dim() == 0:
+                            return constructor().resize_as_(grad)
+                        return constructor(grad_indices, values, grad.size())
+
+                    acc = state[_key(0)]
+                    update_values = _compute_sparse_update(beta, acc, grad_values, grad_indices)
+
+                    self._update_sparse_accumulator(beta, acc, make_sparse(update_values))
+
+                    # Add small amount for numerical stability
+                    update_values.add_(eps).rsqrt_().mul_(grad_values)
+
+                    update = make_sparse(update_values)
+                else:
+                    # Get previous accumulators mu_{t-1}
+                    if rank > 1:
+                        acc_list = [state[_key(i)] for i in range(rank)]
+                    else:
+                        acc_list = [state[_key(0)]]
+
+                    # Get update from accumulators and gradients
+                    update = _compute_update(beta, acc_list, grad)
+
+                    # Update accumulators.
+                    self._update_accumulator(beta, acc_list, update)
+
+                    # Add small amount for numerical stability
+                    update.add_(eps).rsqrt_().mul_(grad)
+
+                    if momentum > 0.:
+                        m = state['momentum_buffer']
+                        update.mul_(1. - momentum).add_(m, alpha=momentum)
+                        state['momentum_buffer'] = update.detach()
+
+                p.sub_(update, alpha=group['lr'])
+                state['step'] += 1
+        return loss
+
+    def _update_accumulator(self, beta, acc_list, update):
+        for i, acc in enumerate(acc_list):
+            nu_max = _max_reduce_except_dim(update, i)
+            if beta > 0.:
+                torch.max(acc, nu_max, out=acc)
+            else:
+                # No need to compare - nu_max is bigger because of grad ** 2
+                acc.copy_(nu_max)
+
+    def _update_sparse_accumulator(self, beta, acc, update):
+        nu_max = _max_reduce_except_dim(update.to_dense(), 0).squeeze()
+        if beta > 0.:
+            torch.max(acc, nu_max, out=acc)
+        else:
+            # No need to compare - nu_max is bigger because of grad ** 2
+            acc.copy_(nu_max)
+
+def _compute_sparse_update(beta, acc, grad_values, grad_indices):
+    # In the sparse case, a single accumulator is used.
+    update_values = torch.gather(acc, 0, grad_indices[0])
+    if beta > 0.:
+        update_values.mul_(beta)
+    update_values.addcmul_(grad_values, grad_values, value=1. - beta)
+    return update_values
+
+def _compute_update(beta, acc_list, grad):
+    rank = len(acc_list)
+    update = acc_list[0].clone()
+    for i in range(1, rank):
+        # We rely on broadcasting to get the proper end shape.
+        update = torch.min(update, acc_list[i])
+    if beta > 0.:
+        update.mul_(beta)
+    update.addcmul_(grad, grad, value=1. - beta)
+
+    return update
+
+def _key(i):
+    # Returns key used for accessing accumulators
+    return 'accumulator_' + str(i)
+
+def _add_initial_accumulators(state, grad):
+    # Creates initial accumulators. For a dense tensor of shape (n1, n2, n3),
+    # then our initial accumulators are of shape (n1, 1, 1), (1, n2, 1) and
+    # (1, 1, n3). For a sparse tensor of shape (n, *), we use a single
+    # accumulator of shape (n,).
+    shape = grad.shape
+    rank = len(shape)
+    defaults = {'device': grad.device, 'dtype': grad.dtype}
+    acc = {}
+
+    if grad.is_sparse:
+        acc[_key(0)] = torch.zeros(shape[0], **defaults)
+    elif rank == 0:
+        # The scalar case is handled separately
+        acc[_key(0)] = torch.zeros(shape, **defaults)
+    else:
+        for i in range(rank):
+            acc_shape = [1] * i + [shape[i]] + [1] * (rank - 1 - i)
+            acc[_key(i)] = torch.zeros(acc_shape, **defaults)
+
+    state.update(acc)
+
+def _max_reduce_except_dim(tensor, dim):
+    # Computes max along all dimensions except the given dim.
+    # If tensor is a scalar, it returns tensor.
+    rank = len(tensor.shape)
+    result = tensor
+    if rank > 0:
+        assert dim < rank
+        for d in range(rank):
+            if d != dim:
+                result = result.max(dim=d, keepdim=True).values
+    return result
+
+
+
+
+
+
+class CAME(torch.optim.Optimizer):
+    """Implements CAME algorithm.
+    This implementation is based on:
+    `CAME: Confidence-guided Adaptive Memory Efficient Optimization`
+    https://arxiv.org/abs/2307.02047
+    https://github.com/yangluo7/CAME
+    based on adafactor with align factor , similar convergence to adam and memory to adafactor
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): external learning rate (default: None)
+        eps (tuple[float, float]): regularization constants for square gradient
+            and instability respectively (default: (1e-30, 1e-16))
+        clip_threshold (float): threshold of root-mean-square of
+            final gradient update (default: 1.0)
+        betas (tuple[float, float, float]): coefficient used for computing running averages of
+        update, square gradient and instability (default: (0.9, 0.999, 0.9999)))
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+    """
+
+    def __init__(
+        self,
+        params,
+        lr=None,
+        eps=(1e-30, 1e-16),
+        clip_threshold=1.0,
+        betas=(0.9, 0.999, 0.9999),
+        weight_decay=0.0,
+    ):
+        assert lr > 0.
+        assert all([0. <= beta <= 1. for beta in betas])
+
+        defaults = dict(
+            lr=lr,
+            eps=eps,
+            clip_threshold=clip_threshold,
+            betas=betas,
+            weight_decay=weight_decay,
+        )
+        super(CAME, self).__init__(params, defaults)
+
+    @property
+    def supports_memory_efficient_fp16(self):
+        return True
+
+    @property
+    def supports_flat_params(self):
+        return False
+
+
+    def _get_options(self, param_shape):
+        factored = len(param_shape) >= 2
+        return factored
+
+    def _rms(self, tensor):
+        return tensor.norm(2) / (tensor.numel() ** 0.5)
+
+    def _approx_sq_grad(self, exp_avg_sq_row, exp_avg_sq_col):
+        r_factor = (
+            (exp_avg_sq_row / exp_avg_sq_row.mean(dim=-1, keepdim=True))
+            .rsqrt_()
+            .unsqueeze(-1)
+        )
+        c_factor = exp_avg_sq_col.unsqueeze(-2).rsqrt()
+        return torch.mul(r_factor, c_factor)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Args:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.dtype in {torch.float16, torch.bfloat16}:
+                    grad = grad.float()
+                if grad.is_sparse:
+                    raise RuntimeError("CAME does not support sparse gradients.")
+
+                state = self.state[p]
+                grad_shape = grad.shape
+
+                factored = self._get_options(grad_shape)
+                # State Initialization
+                if len(state) == 0:
+                    state["step"] = 0
+
+                    state["exp_avg"] = torch.zeros_like(grad)
+                    if factored:
+                        state["exp_avg_sq_row"] = torch.zeros(grad_shape[:-1]).type_as(grad)
+                        state["exp_avg_sq_col"] = torch.zeros(
+                            grad_shape[:-2] + grad_shape[-1:]
+                        ).type_as(grad)
+
+                        state["exp_avg_res_row"] = torch.zeros(grad_shape[:-1]).type_as(grad)
+                        state["exp_avg_res_col"] = torch.zeros(
+                            grad_shape[:-2] + grad_shape[-1:]
+                        ).type_as(grad)
+                    else:
+                        state["exp_avg_sq"] = torch.zeros_like(grad)
+
+                    state["RMS"] = 0
+
+                state["step"] += 1
+                state["RMS"] = self._rms(p.data)
+
+                update = (grad**2) + group["eps"][0]
+                if factored:
+                    exp_avg_sq_row = state["exp_avg_sq_row"]
+                    exp_avg_sq_col = state["exp_avg_sq_col"]
+
+                    exp_avg_sq_row.mul_(group["betas"][1]).add_(
+                        update.mean(dim=-1), alpha=1.0 - group["betas"][1]
+                    )
+                    exp_avg_sq_col.mul_(group["betas"][1]).add_(
+                        update.mean(dim=-2), alpha=1.0 - group["betas"][1]
+                    )
+
+                    # Approximation of exponential moving average of square of gradient
+                    update = self._approx_sq_grad(exp_avg_sq_row, exp_avg_sq_col)
+                    update.mul_(grad)
+                else:
+                    exp_avg_sq = state["exp_avg_sq"]
+
+                    exp_avg_sq.mul_(group["betas"][1]).add_(update, alpha=1.0 - group["betas"][1])
+                    update = exp_avg_sq.rsqrt().mul_(grad)
+
+                update.div_(
+                    (self._rms(update) / group["clip_threshold"]).clamp_(min=1.0)
+                )
+
+                exp_avg = state["exp_avg"]
+                exp_avg.mul_(group["betas"][0]).add_(update, alpha=1 - group["betas"][0])
+
+                # Confidence-guided strategy
+                # Calculation of instability
+                res = (update - exp_avg)**2 + group["eps"][1]
+
+                if factored:
+                    exp_avg_res_row = state["exp_avg_res_row"]
+                    exp_avg_res_col = state["exp_avg_res_col"]
+
+                    exp_avg_res_row.mul_(group["betas"][2]).add_(
+                        res.mean(dim=-1), alpha=1.0 - group["betas"][2]
+                    )
+                    exp_avg_res_col.mul_(group["betas"][2]).add_(
+                        res.mean(dim=-2), alpha=1.0 - group["betas"][2]
+                    )
+
+                    # Approximation of exponential moving average of instability
+                    res_approx = self._approx_sq_grad(exp_avg_res_row, exp_avg_res_col)
+                    update = res_approx.mul_(exp_avg)
+                else:
+                    update = exp_avg.clone()
+
+                if group["weight_decay"] != 0:
+                    p.data.add_(
+                            p.data, alpha=-group["weight_decay"] * group["lr"]
+                        )
+
+                update.mul_(group["lr"])
+                p.data.add_(-update)
+
+        return loss
+    
+
+
+
+# Define the Lion Optimizer class
+class Lion_singlescale(Optimizer):
+    r"""Implements Lion algorithm."""
+
+    def __init__(self, params, lr=1e-4, betas=(0.95, 0.98), scaling = None, weight_decay=0.0, nesterov_momentum = 0):
+        """
+        Initialize the hyperparameters.
+        Adapted from https://github.com/xnchxy/GeneralSGD
+        Based on the https://arxiv.org/abs/2307.10053 and https://arxiv.org/abs/2302.06675
+        Args:
+            params (iterable): iterable of parameters to optimize or dicts defining
+                parameter groups
+            lr (float, optional): learning rate (default: 1e-4)
+            betas (Tuple[float, float], optional): coefficients used for computing
+                running averages of gradients (default: (0.9, 0.99))
+            weight_decay (float, optional): weight decay coefficient (default: 0)
+        """
+
+        # Validate input hyperparameters
+        if not 0.0 <=  lr:
+            raise ValueError('Invalid learning rate: {}'.format(lr))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError('Invalid beta parameter at index 0: {}'.format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError('Invalid beta parameter at index 1: {}'.format(betas[1]))
+        
+        # if scaling is None:
+        #     scaling = 1.0 /lr
+        #     if not 0.0 <= scaling:
+        #         raise ValueError('Invalid learning rate: {}'.format(scaling))
+            
+        # Set default values for the optimizer
+        
+        defaults = dict(lr=lr, betas=betas, weight_decay=weight_decay, scaling = scaling, nesterov_momentum = nesterov_momentum)
+        super().__init__(params, defaults)
+
+        for group in self.param_groups:
+            if group['scaling'] is None:
+                group['scaling'] = 1.0 / group['lr']
+                if not 0.0 <= group['scaling'] :
+                    raise ValueError('Invalid scaling parameter: {}'.format(scaling))
+
+    # Define the step function for the optimizer
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        # Iterate through parameter groups and update each parameter
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                # Perform stepweight decay
+                p.data.mul_(1 - group['lr'] * group['weight_decay'])
+
+                grad = p.grad
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p)
+
+                exp_avg = state['exp_avg']
+                beta1, beta2 = group['betas']
+                scaling = group['scaling']
+
+                tau1 = (1-beta1) * scaling
+                tau2 = (1-beta2) * scaling
+                lr = group['lr']
+                nesterov_momentum = group['nesterov_momentum']
+
+                momentum_param1 = max(1-lr * tau1, beta1)
+                momentum_param2 = max(1-lr * tau2, beta2)
+
+                # Weight update
+                update = exp_avg * momentum_param1 + grad * (1 - momentum_param1)
+                if nesterov_momentum >1e-5:
+                    p.add_(torch.sign(update) + nesterov_momentum * grad, alpha=-group['lr'])
+                else:
+                    p.add_(torch.sign(update), alpha=-group['lr'])
+
+                # Decay the momentum running average coefficient
+                exp_avg.mul_(momentum_param2).add_(grad, alpha=1 - momentum_param2)
+
+        return loss
